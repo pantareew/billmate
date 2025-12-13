37dda7cab1c4ecc628a20cbf9b66992f1ad34c7e
pantareew <pantaree1459@gmail.com>
2025-12-13 22:56:25 +1100

    upload new bill ui

--- frontend/app/bills/new/page.tsx
+++ frontend/app/bills/new/page.tsx
@@ -115,7 +115,7 @@ export default function NewBillPage() {
     }
   };
   return (
-    <div className="max-w-3xl mx-auto px-6 space-y-6 pt-6 pb-40">
+    <div className="max-w-3xl mx-auto px-6 space-y-6 pt-6 pb-15 mt-5 mb-20 bg-white rounded-xl shadow-md">
       {/*upload mode */}
       {mode === "upload" ? (
         <div className="space-y-2">
@@ -124,7 +124,7 @@ export default function NewBillPage() {
             {/*toggle to manual mode */}
             <button
               onClick={() => setMode("manual")}
-              className="border-[#146ff7] border-2 text-[#146ff7] hover:bg-[#216edf] hover:text-white px-4 py-2 rounded cursor-pointer"
+              className="border-[#146ff7] border-2 text-[#146ff7] hover:bg-[#146ff7] hover:text-white px-4 py-2 rounded-lg cursor-pointer"
             >
               Create Bill Manually
             </button>
@@ -135,7 +135,7 @@ export default function NewBillPage() {
           {/*upload area */}
           {/*when this div box is clicked, it will open (from .click()) element with id fileInput (which is input type file)*/}
           <div
-            className="border-2 border-dashed border-indigo-300 bg-indigo-50 rounded-md p-20 flex flex-col items-center justify-center cursor-pointer mb-6"
+            className="border-2 border-dashed border-[#146ff7] bg-blue-50 rounded-md px-20 py-5 flex flex-col items-center justify-center cursor-pointer mb-6"
             onClick={() => document.getElementById("fileInput")?.click()}
           >
             {/*preview the selected file or upload the file*/}
@@ -143,10 +143,10 @@ export default function NewBillPage() {
               <img
                 src={URL.createObjectURL(file)}
                 alt="Receipt Preview"
-                className="max-h-64"
+                className="max-h-80"
               />
             ) : (
-              <div className="text-center">
+              <div className="text-center py-30">
                 <Camera size={40} className="text-gray-400 mx-auto mb-2" />
                 <p className="text-gray-400 mb-1"> Click to upload receipt</p>
                 <p className="text-gray-400/70 text-sm">
@@ -155,7 +155,7 @@ export default function NewBillPage() {
               </div>
             )}
             {loading && (
-              <p className="text-amber-700 flex items-center gap-2 my-4">
+              <p className="text-amber-500 flex items-center gap-2 my-4">
                 <Loader size={25} /> Processing with Open AI
               </p>
             )}
@@ -171,51 +171,82 @@ export default function NewBillPage() {
           {aiResult && (
             <>
               {/*show data from AI */}
-              <div className="bg-orange-100 border border-orange-300 rounded-xl p-4 mt-4">
-                <div className="text-amber-700 flex items-center gap-2 mb-2 font-semibold">
-                  <CircleCheckBig size={25} />
-                  <h3>Receipt Extracted</h3>
+              <div className="bg-[#5b85f4] border border-[#a1b9f9] rounded-xl p-4 mt-4 shadow-sm">
+                <div className="flex items-center gap-2 mb-2 font-semibold text-white">
+                  <CircleCheckBig size={20} />
+                  <h3 className="text-md">Receipt Extracted</h3>
                 </div>
-                <p className="text-orange-900 flex justify-between">
+                <p className="text-white flex justify-between text-sm">
                   <span className="font-medium">Merchant: </span>
                   <span>{aiResult.title}</span>
                 </p>
-                <p className="text-orange-900 flex justify-between">
+                <p className="text-white flex justify-between text-sm mt-1">
                   <span className="font-medium">Total: </span>
                   <span>${aiResult.total_amount}</span>
                 </p>
               </div>
               {/*show groups for selection */}
-              <select onChange={(e) => setSelectedGroup(e.target.value)}>
-                <option>Select a group</option>
-                {groups.map((group) => (
-                  <option key={group.id} value={group.id}>
-                    {group.name}
-                  </option>
-                ))}
-              </select>
+              <div>
+                <h3 className="text-md font-semibold mt-3 mb-2 text-gray-700">
+                  Select a group
+                </h3>
+                <div className="space-y-4">
+                  {groups.map((group) => (
+                    <div
+                      key={group.id}
+                      className={`p-4 rounded-lg cursor-pointer border-2 transition shadow-sm text-[15px] ${
+                        selectedGroup === group.id
+                          ? "border-violet-400/80 bg-violet-400 text-white"
+                          : "border-violet-400/80 bg-white hover:bg-violet-400 hover:border-violet-400/80 hover:text-white text-violet-600"
+                      }`}
+                      onClick={() => setSelectedGroup(group.id)}
+                    >
+                      <p className="font-medium">{group.name}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
               {/*select users */}
-              {members.map((member) => (
-                <label key={member.id} className="block">
-                  <input
-                    type="checkbox"
-                    checked={selectedMembers.includes(member.id)} //tick the box if user is in selectedMembers array
-                    onChange={() =>
-                      setSelectedMembers(
-                        (prev) =>
-                          prev.includes(member.id) //check if current array has this user
-                            ? prev.filter((id) => id !== member.id) //remove user if already selected
-                            : [...prev, member.id] //add user if not selected
-                      )
-                    }
-                  />
-                  {member.name}
-                </label>
-              ))}
+              <div>
+                <h3 className="text-md font-semibold mt-3 mb-2 text-gray-700">
+                  Split Among
+                </h3>
+                <div className="flex space-x-6">
+                  {members.map((member) => {
+                    const isChecked = selectedMembers.includes(member.id); //check if user is in selectedMembers array
+                    return (
+                      <label
+                        key={member.id}
+                        className={`px-4 py-1 rounded-full cursor-pointer shadow-sm border text-sm ${
+                          isChecked
+                            ? "bg-[#cc8cb6] text-white border-[#cc8cb6] hover:bg-white hover:text-[#cc8cb6]"
+                            : "bg-white text-[#cc8cb6] hover:bg-[#cc8cb6] hover:text-white hover:border-[#cc8cb6]"
+                        }`}
+                      >
+                        <input
+                          hidden
+                          className="mr-2"
+                          type="checkbox"
+                          checked={isChecked} //tick the box if user is in selectedMembers
+                          onChange={() =>
+                            setSelectedMembers(
+                              (prev) =>
+                                isChecked //check if current array has this user
+                                  ? prev.filter((id) => id !== member.id) //remove user if already selected
+                                  : [...prev, member.id] //add user if not selected
+                            )
+                          }
+                        />
+                        {member.name}
+                      </label>
+                    );
+                  })}
+                </div>
+              </div>
               {/*split and create bill button*/}
               {selectedGroup && selectedMembers.length > 0 && (
                 <button
-                  className="bg-cyan-500 p-4 rounded text-white cursor-pointer hover:bg-cyan-600"
+                  className="float-right bg-indigo-500 py-2 px-4 rounded-lg text-white cursor-pointer hover:bg-indigo-600"
                   onClick={handleSplit}
                 >
                   {splitting ? "Processing the bill..." : "Split & Create Bill"}
