bc9e1d79565ccb2446850364ddcbfb22da6863b6
pantareew <pantaree1459@gmail.com>
2026-02-14 22:11:36 +1100

    create ItemSplit.tsx

---
+++ frontend/components/ItemSplit.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useState } from "react";
+
+type Member = {
+  id: string;
+  name: string;
+};
+
+type Item = {
+  id: string;
+  name: string;
+  price: number;
+};
+
+export default function ItemSplit() {
+  //track which members are assigned to each item
+}
