1f4fe9cd663d687f46faf830dc911340e9236df5
pantareew <pantaree1459@gmail.com>
2026-02-19 11:39:19 +1100

    toggleAssign function

--- frontend/components/ItemSplit.tsx
+++ frontend/components/ItemSplit.tsx
@@ -24,8 +24,20 @@ export default function ItemSplit({
   members,
   onComplete,
 }: ItemSplitProps) {
-  //track which members are assigned to which item
+  //state var to track which members are assigned to an item
   const [assign, setAssign] = useState<
     Record<string, string[]> //itemId, members
   >(() => Object.fromEntries(items.map((item) => [item.id, []]))); //{itemId: []}
+
+  //toggle assign when click a member on an item
+  const toggleAssign = (itemId: string, memberId: string) => {
+    //update assign var
+    setAssign((prev) => {
+      const current = prev[itemId] || []; //get current members of that item
+      const updated = current.includes(memberId) //check if clicked member is already selected
+        ? current.filter((id) => id !== memberId) //remove member if already selected
+        : [...current, memberId]; //add member if not
+      return { ...prev, [itemId]: updated }; //crete a new state object (to re render UI) and overwrite value for itemId with updated members
+    });
+  };
 }
